87f39baf9d278a2b25f4fd22c775a622f3f6bf39
Ed  Borodin <edborodin@MacBook-Pro-Ed.local>
2019-02-08 23:31:46 +0200

    Added reload script, styles

--- src/js/app.js
+++ src/js/app.js
@@ -19,62 +19,61 @@ $(document).ready(function () {
             speed:                      700,      // speed in milliseconds it will take to rotate from one to the next
             animationEasing:            'linear', // the easing effect to use when animating
         });
-
         $('#prev').bind('click', function () {
             carousel.prev();
             return false
         });
-
         $('#next').bind('click', function () {
             carousel.next();
             return false;
         });
     } else if
-        ($(window).width() >= 768 && $(window).width() <= 991.98)
+        ($(window).width() >= 768 || $(window).width() <= 991.98)
         {
             let carousel = $('#carousel').waterwheelCarousel({
                 // number tweeks to change apperance
-                separation: 127, // distance between items in carousel
-                horizonOffsetMultiplier: 1,   // multipled by horizon offset to increase/decrease offset for each additional item
-                sizeMultiplier: 0.78, // determines how drastically the size of each item changes
-                opacityMultiplier: 1, // determines how drastically the opacity of each item changes
-                flankingItems: 1,   // the number of items visible on either side of the center
+                separation: 127,
+                horizonOffsetMultiplier: 1,
+                sizeMultiplier: 0.78,
+                opacityMultiplier: 1,
+                flankingItems: 1,
                 // animation
-                speed: 700,      // speed in milliseconds it will take to rotate from one to the next
-                animationEasing: 'linear', // the easing effect to use when animating
+                speed: 700,
+                animationEasing: 'linear',
             });
-
             $('#prev').bind('click', function () {
                 carousel.prev();
                 return false
             });
-
             $('#next').bind('click', function () {
                 carousel.next();
                 return false;
             });
-        }
-    else {
+        } else {
         let carousel = $('#carousel').waterwheelCarousel({
             // number tweeks to change apperance
-            separation: 197, // distance between items in carousel
-            horizonOffsetMultiplier: 1,   // multipled by horizon offset to increase/decrease offset for each additional item
-            sizeMultiplier: 0.78, // determines how drastically the size of each item changes
-            opacityMultiplier: 1, // determines how drastically the opacity of each item changes
-            flankingItems: 2,   // the number of items visible on either side of the center
+            separation: 197,
+            horizonOffsetMultiplier: 1,
+            sizeMultiplier: 0.78,
+            opacityMultiplier: 1,
+            flankingItems: 2,
             // animation
-            speed: 700,      // speed in milliseconds it will take to rotate from one to the next
-            animationEasing: 'linear', // the easing effect to use when animating
+            speed: 700,
+            animationEasing: 'linear',
         });
-
         $('#prev').bind('click', function () {
             carousel.prev();
             return false
         });
-
         $('#next').bind('click', function () {
             carousel.next();
             return false;
         });
     }
+    $(window).on('resize', function(){
+        var win = $(this); //this = window
+        if (win.width() < 1400) {
+            location.reload();
+        }
+    });
 });
